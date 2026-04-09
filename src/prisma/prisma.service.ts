b2d3c97581502394da982e5766d69de73f15c2a5
Marcin Berus <marcinberus@hotmail.com>
2026-04-09 11:42:56 +0200

    fix formatting

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,10 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor(private readonly configService: ConfigService) {
     const connectionString = configService.get<string>('DATABASE_URL');
 
