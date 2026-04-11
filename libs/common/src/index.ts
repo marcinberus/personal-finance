13da500dabe13da71c34c7d37365a1a475a9825b
Marcin Berus <marcinberus@hotmail.com>
2026-04-12 00:58:33 +0200

    shared jwt strategy

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,5 @@
 export { CurrentUser } from './decorators/current-user.decorator';
 export type { AuthenticatedUser } from './decorators/current-user.decorator';
 export { JwtGuard } from './guards/jwt.guard';
+export { JwtAuthModule } from './auth/jwt-auth.module';
+export type { JwtAuthModuleOptions } from './auth/jwt-auth.module';
